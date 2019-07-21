ce3c944e5b2d8feedd2e24e868c0e8e6d8bf8763
Alex van Steenhoven <alex.steenhoven@gmail.com>
2019-07-21 23:04:00 +0200

    Finished the tail of the snake

--- main.js
+++ main.js
@@ -10,12 +10,18 @@ const columns = canvas.width / scale;
 let snake;
 (function setup() {
   snake = new Snake();
-  snake.draw();
+  fruit = new Fruit();
+  fruit.randomLocation();
 
   window.setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     snake.update();
+    fruit.draw();
     snake.draw();
+
+    if (snake.eat(fruit)) {
+      fruit.randomLocation();
+    }
   }, 250);
 })();
 
